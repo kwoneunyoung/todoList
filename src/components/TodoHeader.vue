81632a6737bb9a1dddebbf23fa0a12536402f218
권노미 <dmsdud4972@naver.com>
2022-12-08 15:43:09 +0900

    create todoList - Header

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <header id="head">
+            <div class="inner">
+                <h1 id="logo">
+                    <a href="javascript:;">Todo</a>
+                </h1>
+                <input type="textarea" placeholder="할 일을 검색 해보세요!">
+                <div id="gnb">
+                    <ul class="nav">
+                        <li class="active"><a href="javascript:;">홈</a></li>
+                        <li><a href="javascript:;">기록</a></li>
+                        <li><a href="javascript:;">캘린더</a></li>
+                    </ul>
+                    <ul class="sign">
+                        <li><a href="javascript:;">권노미님</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+import ('../assets/css/style.css');
+export default {
+    name : "TodoHeader",
+    components : {
+    },
+}
+</script>
+
+<style>
+
+</style>
